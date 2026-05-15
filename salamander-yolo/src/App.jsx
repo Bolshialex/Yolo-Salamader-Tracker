98d037e6dbfbbf26f6b0940bb527a4f8a2d13d5d
Kimberly Mageary <kimberlymageary@gmail.com>
2026-05-14 17:35:06 -0700

    Implemented logic to fetch video

--- salamander-yolo/src/App.jsx
+++ salamander-yolo/src/App.jsx
@@ -1,15 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import FilePicker from './components/FilePicker';
 import './App.css'
 
 function App() {
-  const [currentFile, setFile] = useState("");
+  const [file, setFile] = useState(null);
   const [response, setResponse] = useState();
 
+
+  useEffect(()=> {
+    async function sendFile() {
+      if(!file) return;
+
+      try{
+        const formData = new FormData();
+        formData.append("video", file);
+        const res = await fetch("http://127.0.0.1:8000/track", {
+          method: "POST",
+          body: formData,
+        });
+
+        const data = await res.json();
+        setResponse(data);
+      } catch(e){
+        console.log(e)
+      }
+    }
+    
+    sendFile()
+
+  }, [file])
 
   return (
     <div className="main">
       <FilePicker onFileSelect={setFile} />
+      {response?.video_url ? (
+        <>
+          <p>Status: {response.status}</p>
+          <video src={response.video_url} controls />
+        </>) : (
+          <p>No video uploaded yet</p>
+        )}
     </ div>
   )
 }
