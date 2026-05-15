b7649cbbfaf9f06cd819645b22f80c822162b70a
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2026-05-15 11:32:14 -0700

    Add changes to form

--- salamander-yolo/src/components/FilePicker.jsx
+++ salamander-yolo/src/components/FilePicker.jsx
@@ -1,22 +1,36 @@
 import { useState } from "react";
 
 export default function FilePicker({ onFileSelect }) {
-    const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
 
-    function handleFileChange(e) {
-        const file = e.target.files[0];
-        setSelectedFile(file);
-        onFileSelect(file);
-    }
+  function handleFileChange(e) {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+    onFileSelect(file);
+  }
 
-    return (
-        <div className="file-picker">
-            <label className="file-picker-label">Drop a file
-                <input type="file" onChange={handleFileChange} />
-            </label>
-            {selectedFile && (
-                <p>Selected: {selectedFile.name}</p>
-            )}
-        </div>
-    )
+  function handleFileNameUpdate(e) {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+    console.log(file.name);
+  }
+
+  return (
+    <div>
+      <form className="sal-form" onSubmit={handleFileChange}>
+        <label className="file-picker" htmlFor="video">
+          <div className="file-picker-label">
+            Drop a File or <span>Browse</span>
+          </div>
+          <input onChange={handleFileNameUpdate} type="file" id="video" />
+          <p>
+            uploaded file: <span>{selectedFile ? selectedFile.name : ""}</span>
+          </p>
+        </label>
+        <button type="submit" className="sal-button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
