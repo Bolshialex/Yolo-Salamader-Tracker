bfe27a14821c181f26b9a9651a0319325da7d778
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2026-05-20 12:30:07 -0700

    Add frontend changes

--- salamander-yolo/src/components/FilePicker.jsx
+++ salamander-yolo/src/components/FilePicker.jsx
@@ -3,30 +3,41 @@ import { useState } from "react";
 export default function FilePicker({ onFileSelect }) {
   const [selectedFile, setSelectedFile] = useState(null);
 
-  function handleFileChange(e) {
-    const file = e.target.files[0];
-    setSelectedFile(file);
-    onFileSelect(file);
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (selectedFile) {
+      onFileSelect(selectedFile);
+    }
   }
 
-  function handleFileNameUpdate(e) {
+  function handleFileChange(e) {
     const file = e.target.files[0];
-    setSelectedFile(file);
-    console.log(file.name);
+
+    if (file) {
+      setSelectedFile(file);
+      console.log(file.name);
+    } else {
+      setSelectedFile(null);
+    }
   }
 
   return (
     <div>
-      <form className="sal-form" onSubmit={handleFileChange}>
+      <form className="sal-form" onSubmit={handleSubmit}>
         <label className="file-picker" htmlFor="video">
           <div className="file-picker-label">
             Drop a File or <span>Browse</span>
           </div>
-          <input onChange={handleFileNameUpdate} type="file" id="video" />
+
+          <input onChange={handleFileChange} type="file" id="video" />
+
           <p>
-            uploaded file: <span>{selectedFile ? selectedFile.name : ""}</span>
+            uploaded file:{" "}
+            <span>{selectedFile ? selectedFile.name : "None"}</span>
           </p>
         </label>
+
         <button type="submit" className="sal-button">
           Submit
         </button>
